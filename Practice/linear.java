19fc8b88dea6fe88f8c8a7c7a061b65730cf8fa9
Abhishek <abhishekphd14@gmail.com>
2022-03-23 22:55:49 +0530

    Update linear.java

--- Practice/linear.java
+++ Practice/linear.java
@@ -1,5 +1,16 @@
-public class linear
-
-public static void main(String[] args) {
+package Practice;
+public class linear {
+    public static void main(String[] args) {
     
-}
+    }
+    // static int linearSearch ( int[] arr, int target) {
+    //     if (arr.length == 0){
+    //         return -1;
+    //     }
+         
+    //     }
+
+    }
+
+
+
