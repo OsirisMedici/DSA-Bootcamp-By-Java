a8b896cfc7f0ea746f5106da96fe742bdd67a825
Abhishek <abhishekphd14@gmail.com>
2022-03-23 22:55:44 +0530

    Create mema.java

---
+++ Practice/mema.java
@@ -0,0 +1,13 @@
+//  package com.abhis;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+
+//             system.out.println("Helooo vaiiiiii");
+//         }
+//     }
+
+// class mema {public
+    
+// }
