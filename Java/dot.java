dd6dc06cf6f00d8e3f3c3b00464a6bffa3d898e3
Abhishek <abhishekphd14@gmail.com>
2022-03-25 12:55:09 +0530

    commit

---
+++ Java/dot.java
@@ -0,0 +1,9 @@
+public class dot {
+
+    public static void main(String[] args) {
+        System.out.println("Hellllo World!!");
+        System.out.println("Javaaaaaaaaaaaaaaa");
+        System.out.println("AbhishekOnIg");
+
+    }
+}
--- Practice/sister.java
+++
@@ -1,6 +0,0 @@
-public class sister {
-    public static void main(String[] args) {
-        System.out.println("helooooooooooooooooooooooooo");
-    }
-
-}
