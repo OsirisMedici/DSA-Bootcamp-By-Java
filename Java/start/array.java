e8250d183d0b8883b7f8ddd1034ec2f56e1ca0a8
Abhishek <abhishekphd14@gmail.com>
2022-03-26 00:12:39 +0530

    Update array.java

--- Java/start/array.java
+++ Java/start/array.java
@@ -1,6 +1,7 @@
 package start;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class array {
     public static void main(String[] args) {
@@ -16,5 +17,22 @@ public class array {
 
         System.out.println(marks[0]);
 
+    }
+
+    public static void sum() {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print(
+                    "Enter number 1: ");
+            int num1 = in.nextInt();
+
+            System.out.print(
+                    "Enter number 2: ");
+            int num2 = in.nextInt();
+
+            int sum = num1 + num2;
+
+            System.out.print("The Sum is : ");
+            System.out.print(sum);
+        }
     }
 }
