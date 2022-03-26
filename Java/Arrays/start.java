8c9cb08146bd9c545b2c47538dcf9db8cb89a288
Abhishek <abhishekphd14@gmail.com>
2022-03-26 19:20:14 +0530

    Create start.java

---
+++ Java/Arrays/start.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+import java.lang.reflect.Array;
+
+public class start {
+    public static void main(String[] args) {
+
+        int[] arr = new int[3];
+
+        arr[0] = 20;
+        arr[1] = 60;
+        arr[2] = 40;
+        System.out.println(arr[2]);
+    }
+}
