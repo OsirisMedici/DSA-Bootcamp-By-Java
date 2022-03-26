f80e8af09dd0753b1b5b1ae6d522f3349725c1ff
Abhishek <abhishekphd14@gmail.com>
2022-03-26 19:20:17 +0530

    Create inp.java

---
+++ Java/Arrays/inp.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class inp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] inp = new int[4];
+        for (int i = 0; i < inp.length; i++) {
+
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < inp.length; i++) {
+            Object[] arr;
+            System.out.println(
+                    arr[i] + " ");
+        }
+
+    }
+}
