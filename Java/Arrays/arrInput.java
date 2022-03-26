75d645ea56c69c349efa6c5d70a550e351543860
Abhishek <abhishekphd14@gmail.com>
2022-03-26 19:20:20 +0530

    Create arrInput.java

---
+++ Java/Arrays/arrInput.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class arrInput {
+    public static void main(String[] args) {
+        System.out.println(
+                "Enter a Number: ");
+
+        try (Scanner in = new Scanner(System.in)) {
+            int[] arr = new int[5];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = in.nextInt();
+                System.out.println(arr[i] + " ");
+            }
+        }
+    }
+}
