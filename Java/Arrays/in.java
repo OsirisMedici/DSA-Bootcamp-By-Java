bd851567aa685f8dd2754f7709b0bc7558db870c
Abhishek <abhishekphd14@gmail.com>
2022-03-27 10:42:11 +0530

    Create in.java

---
+++ Java/Arrays/in.java
@@ -0,0 +1,17 @@
+
+public class in {
+
+    /**
+     *
+     */
+    private static final int _0 = 0;
+    /**
+     *
+     */
+    private static final int INT = _0;
+
+    public static int nextInt() {
+        return INT;
+    }
+
+}
